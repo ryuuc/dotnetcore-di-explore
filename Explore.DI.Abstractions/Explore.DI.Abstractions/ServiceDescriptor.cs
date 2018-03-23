c7ccde3d1d926aeeafb7fc67a00c1b81fea6e5f3
liuchao <Ry2u@outlook.com>
2018-03-23 16:07:25 +0800

    ServiceDescriptor,ServiceLifetime

---
+++ Explore.DI.Abstractions/Explore.DI.Abstractions/ServiceDescriptor.cs
@@ -0,0 +1,288 @@
+﻿namespace Explore.DI
+{
+    /// <summary>
+    /// 服务描述器，包含服务类型、服务实例及生命周期等信息
+    /// </summary>
+    public class ServiceDescriptor
+    {
+        public System.Type ImplementationType { get; }
+        public System.Type ServiceType { get; }
+        public ServiceLifetime LifeTime { get; }
+        public object ImplementationInstance { get; }
+        public System.Func<System.IServiceProvider, object> ImplementationFactory { get; }
+        internal System.Type GetImplementationType()
+        {
+            if (ImplementationType != null)
+            {
+                return ImplementationType;
+            }
+            else if (ImplementationInstance != null)
+            {
+                return ImplementationInstance.GetType();
+            }
+            else if (ImplementationFactory != null)
+            {
+                var typeArguments = ImplementationFactory.GetType().GenericTypeArguments;
+
+                System.Diagnostics.Debug.Assert(typeArguments.Length == 2);
+
+                return typeArguments[1];
+            }
+
+            System.Diagnostics.Debug.Assert(false, "ImplementationType, ImplementationInstance or ImplementationFactory must be non null");
+
+            return null;
+        }
+
+        #region ctor
+        public ServiceDescriptor(System.Type serviceType, System.Type implementationType, ServiceLifetime lifetime)
+            : this(serviceType, lifetime)
+        {
+            if (serviceType == null)
+            {
+                throw new System.ArgumentNullException(nameof(serviceType));
+            }
+
+            ImplementationType = implementationType ?? throw new System.ArgumentNullException(nameof(implementationType));
+        }
+
+        public ServiceDescriptor(System.Type servicetype, object instance)
+            : this(servicetype, ServiceLifetime.Singleton)
+        {
+            if (servicetype == null)
+            {
+                throw new System.ArgumentNullException(nameof(servicetype));
+            }
+
+            ImplementationInstance = instance ?? throw new System.ArgumentNullException(nameof(instance));
+        }
+
+        public ServiceDescriptor(System.Type serviceType, System.Func<System.IServiceProvider, object> factory, ServiceLifetime lifetime)
+            : this(serviceType, lifetime)
+        {
+            if (serviceType == null)
+            {
+                throw new System.ArgumentNullException(nameof(serviceType));
+            }
+
+            ImplementationFactory = factory ?? throw new System.ArgumentNullException(nameof(factory));
+        }
+
+        public ServiceDescriptor(System.Type serviceType, ServiceLifetime lifetime)
+        {
+            ServiceType = serviceType;
+
+            LifeTime = lifetime;
+        }
+        #endregion ctor
+
+        #region Transient
+        public static ServiceDescriptor Transient<TSerivice, TImplementation>()
+            where TSerivice : class
+            where TImplementation : class, TSerivice
+        {
+            return Descriptor<TSerivice, TImplementation>(ServiceLifetime.Transient);
+        }
+
+        public static ServiceDescriptor Transient(System.Type serviceType, System.Type implementationType)
+        {
+            if (serviceType == null)
+            {
+                throw new System.ArgumentNullException(nameof(serviceType));
+            }
+
+            if (implementationType == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationType));
+            }
+
+            return Descriptor(serviceType, implementationType, ServiceLifetime.Transient);
+        }
+
+        public static ServiceDescriptor Transient<TService, TImplementation>(System.Func<System.IServiceProvider, TImplementation> implementationFactory)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            if (implementationFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationFactory));
+            }
+
+            return Descriptor(typeof(TService), typeof(TImplementation), ServiceLifetime.Transient);
+        }
+
+        public static ServiceDescriptor Transient(System.Type serviceType, System.Func<System.IServiceProvider, object> implementationFactory)
+        {
+            if (serviceType == null)
+            {
+                throw new System.ArgumentNullException(nameof(serviceType));
+            }
+
+            if (implementationFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationFactory));
+            }
+
+            return Descriptor(serviceType, implementationFactory, ServiceLifetime.Transient);
+        }
+        #endregion Transient
+
+        #region Scoped
+        public static ServiceDescriptor Scoped<TService, TImplementation>()
+            where TService : class
+            where TImplementation : class, TService
+        {
+            return Descriptor<TService, TImplementation>(ServiceLifetime.Scoped);
+        }
+
+        public static ServiceDescriptor Scoped<TService>(System.Func<System.IServiceProvider, TService> implementationFactory)
+            where TService : class
+        {
+            if (implementationFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationFactory));
+            }
+            return Descriptor(typeof(TService), implementationFactory, ServiceLifetime.Scoped);
+        }
+
+        public static ServiceDescriptor Scoped(System.Type serviceType, System.Type implementationType)
+        {
+            return Descriptor(serviceType, implementationType, ServiceLifetime.Scoped);
+        }
+
+        public static ServiceDescriptor Scoped<TService, TImplementation>(System.Func<System.IServiceProvider, TImplementation> implementationFactory)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            if (implementationFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationFactory));
+            }
+
+            return Descriptor(typeof(TService), implementationFactory, ServiceLifetime.Scoped);
+        }
+
+        public static ServiceDescriptor Scoped(System.Type serviceType, System.Func<System.IServiceProvider, object> implementationFactory)
+        {
+            if (serviceType == null)
+            {
+                throw new System.ArgumentNullException(nameof(serviceType));
+            }
+
+            if (implementationFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationFactory));
+            }
+
+            return Descriptor(serviceType, implementationFactory, ServiceLifetime.Scoped);
+        }
+        #endregion Scoped
+
+        #region Singleton
+        public static ServiceDescriptor Singleton<TService, TImplementation>()
+            where TService : class
+            where TImplementation : class, TService
+        {
+            return Descriptor<TService, TImplementation>(ServiceLifetime.Singleton);
+        }
+
+        public static ServiceDescriptor Singleton(System.Type serviceType, System.Type implementationType)
+        {
+            if (serviceType == null)
+            {
+                throw new System.ArgumentNullException(nameof(serviceType));
+            }
+
+            if (implementationType == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationType));
+            }
+
+            return Descriptor(serviceType, implementationType, ServiceLifetime.Singleton);
+        }
+
+        public static ServiceDescriptor Singleton<TService, TImplementation>(System.Func<System.IServiceProvider, TImplementation> implementationFactory)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            if (implementationFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationFactory));
+            }
+
+            return Descriptor(typeof(TService), implementationFactory, ServiceLifetime.Singleton);
+        }
+
+        public static ServiceDescriptor Singleton<TService>(System.Func<System.IServiceProvider, TService> implementationFactory)
+            where TService : class
+        {
+            if (implementationFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationFactory));
+            }
+
+            return Descriptor(typeof(TService), implementationFactory, ServiceLifetime.Singleton);
+        }
+
+        public static ServiceDescriptor Singleton(System.Type serviceType, System.Func<System.IServiceProvider, object> implementationFactory)
+        {
+            if (serviceType == null)
+            {
+                throw new System.ArgumentNullException(nameof(serviceType));
+            }
+
+            if (implementationFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationFactory));
+            }
+
+            return Descriptor(serviceType, implementationFactory, ServiceLifetime.Singleton);
+        }
+
+        public static ServiceDescriptor Singleton<TService>(TService implementationInstance)
+            where TService : class
+        {
+            if (implementationInstance == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationInstance));
+            }
+
+            return Singleton(typeof(TService), implementationInstance);
+        }
+
+        public static ServiceDescriptor Singleton(System.Type serviceType, object implementationInstance)
+        {
+            if (serviceType == null)
+            {
+                throw new System.ArgumentNullException(nameof(serviceType));
+            }
+
+            if (implementationInstance == null)
+            {
+                throw new System.ArgumentNullException(nameof(implementationInstance));
+            }
+
+            return new ServiceDescriptor(serviceType, implementationInstance);
+        }
+        #endregion Singleton
+
+        #region Descriptor
+        private static ServiceDescriptor Descriptor<TService, TImplementation>(ServiceLifetime lifetime)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            return Descriptor(typeof(TService), typeof(TImplementation), lifetime: lifetime);
+        }
+
+        public static ServiceDescriptor Descriptor(System.Type serviceType, System.Type implementationType, ServiceLifetime lifetime)
+        {
+            return new ServiceDescriptor(serviceType, implementationType, lifetime);
+        }
+
+        public static ServiceDescriptor Descriptor(System.Type serviceType, System.Func<System.IServiceProvider, object> implementationFactory, ServiceLifetime lifetime)
+        {
+            return new ServiceDescriptor(serviceType, implementationFactory, lifetime);
+        }
+        #endregion Descriptor
+    }
+}
---
+++ Explore.DI.Abstractions/Explore.DI.Abstractions/ServiceLifetime.cs
@@ -0,0 +1,30 @@
+﻿namespace Explore.DI
+{
+    /// <summary>
+    /// 服务生命周期
+    /// </summary>
+    public enum ServiceLifetime
+    {
+        /// <summary>
+        /// 单例
+        /// 服务在第一次被请求时创建，后续请求使用同一实例
+        /// </summary>
+        Singleton,
+
+        /// <summary>
+        /// 作用域(TBD)
+        /// Specifies that a new instance of the service will be created for each scope.
+        /// <remarks>
+        /// In ASP.NET Core applications a scope is created around each server request.
+        /// </remarks>
+        /// </summary>
+        Scoped,
+
+        /// <summary>
+        /// 瞬态
+        /// 服务在每次请求时都会创建（every time it is requested），
+        /// 适合轻量级（lightweight）、无状态（stateless）的服务。
+        /// </summary>
+        Transient
+    }
+}
