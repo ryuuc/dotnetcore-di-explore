08acfaace3507c93aea855d3b3c11dc0a5e3a6cf
liuchao <ry2u@outlook.com>
2018-03-26 22:33:26 +0800

    ServiceCollectionDescriptorExtensions

---
+++ Explore.DI.Abstractions/Explore.DI.Abstractions/ServiceCollectionDescriptorExtensions/ServiceCollectionDescriptorExtensions.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Explore.DI.Abstractions.ServiceCollectionDescriptorExtensions
+{
+    public static class ServiceCollectionDescriptorExtensions
+    {
+        public static IServiceCollection Add(this IServiceCollection collection, ServiceDescriptor descriptor)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (descriptor == null)
+            {
+                throw new ArgumentNullException(nameof(descriptor));
+            }
+
+            collection.Add(descriptor);
+            return collection;
+        }
+
+        public static IServiceCollection Add(this IServiceCollection collection, IEnumerable<ServiceDescriptor> descriptors)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (descriptors == null)
+            {
+                throw new ArgumentNullException(nameof(descriptors));
+            }
+
+            foreach (var descriptor in descriptors)
+            {
+                collection.Add(descriptors);
+            }
+
+            return collection;
+        }
+
+        public static void TryAdd(this IServiceCollection collection, ServiceDescriptor descriptor)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (descriptor == null)
+            {
+                throw new ArgumentNullException(nameof(descriptor));
+            }
+
+            if (!collection.Any(d => d.ServiceType == descriptor.ServiceType))
+            {
+                collection.Add(descriptor);
+            }
+        }
+
+        public static void TryAdd(this IServiceCollection collection, IEnumerable<ServiceDescriptor> descriptors)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (descriptors == null)
+            {
+                throw new ArgumentNullException(nameof(descriptors));
+            }
+
+            foreach (var d in descriptors)
+            {
+                collection.TryAdd(d);
+            }
+        }
+
+        #region Transient
+
+        public static void TryAddTransient(this IServiceCollection collection,Type service,Type implementationType)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            if (implementationType == null)
+            {
+                throw new ArgumentNullException(nameof(implementationType));
+            }
+
+            var descriptor = ServiceDescriptor.Transient(service, implementationType);
+            TryAdd(collection, descriptor);
+        }
+
+        public static void TryAddTransient(this IServiceCollection collection,Type serviceType,Func<IServiceProvider,object> implementationFactory)
+        {
+            if(collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if(serviceType == null)
+            {
+                throw new ArgumentNullException(nameof(serviceType));
+            }
+
+            if (implementationFactory == null)
+            {
+                throw new ArgumentNullException(nameof(implementationFactory));
+            }
+
+            var descriptor = ServiceDescriptor.Transient(serviceType, implementationFactory);
+            TryAdd(collection, descriptor);
+        }
+
+        #endregion
+
+    }
+}
