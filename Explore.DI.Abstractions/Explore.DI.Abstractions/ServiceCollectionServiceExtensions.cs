15ac3e852426b223415981924a987c6585989237
liuchao <Ry2u@outlook.com>
2018-03-26 16:44:50 +0800

    servicecollection extensions

--- Explore.DI.Abstractions/Explore.DI.Abstractions/ServiceCollectionServiceExtensions.cs
+++ Explore.DI.Abstractions/Explore.DI.Abstractions/ServiceCollectionServiceExtensions.cs
@@ -7,31 +7,18 @@ namespace Explore.DI.Abstractions
 {
     public static class ServiceCollectionServiceExtensions
     {
-        public static IServiceCollection AddTransient(this IServiceCollection services, Type serviceType, Type implementationType)
-        {
-            if (services == null)
-            {
-                throw new ArgumentNullException(nameof(services));
-            }
-
-            if (serviceType == null)
-            {
-                throw new ArgumentNullException(nameof(serviceType));
-            }
-
-            if (implementationType == null)
-            {
-                throw new ArgumentNullException(nameof(implementationType));
-            }
-
-            return Add(services, serviceType, implementationType, ServiceLifetime.Transient);
-        }
-
-        private static IServiceCollection Add(IServiceCollection services, Type serviceType, Type implementationType, ServiceLifetime lifetime)
+        private static IServiceCollection Add(IServiceCollection collection, Type serviceType, Type implementationType, ServiceLifetime lifetime)
         {
             var descriptor = new ServiceDescriptor(serviceType, implementationType, lifetime);
-            services.Add(descriptor);
-            return services;
+            collection.Add(descriptor);
+            return collection;
+        }
+
+        private static IServiceCollection Add(IServiceCollection collection, Type serviceType, Func<IServiceProvider, object> implementationFactory, ServiceLifetime lifetime)
+        {
+            var descriptor = new ServiceDescriptor(serviceType, implementationFactory, lifetime);
+            collection.Add(descriptor);
+            return collection;
         }
 
         public static void TryAdd(this IServiceCollection collection, ServiceDescriptor descriptor)
@@ -71,6 +58,26 @@ namespace Explore.DI.Abstractions
         }
 
         #region transient
+        public static IServiceCollection AddTransient(this IServiceCollection collection, Type serviceType, Type implementationType)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException(nameof(serviceType));
+            }
+
+            if (implementationType == null)
+            {
+                throw new ArgumentNullException(nameof(implementationType));
+            }
+
+            return Add(collection, serviceType, implementationType, ServiceLifetime.Transient);
+        }
+
         public static void TryAddTransient(this IServiceCollection collection, Type serviceType)
         {
             if (collection == null)
@@ -142,11 +149,11 @@ namespace Explore.DI.Abstractions
             TryAddTransient(collection, typeof(TService), typeof(TService));
         }
 
-        public static void TryAddTransient<TService,TImplementation>(this IServiceCollection collection)
-            where TService:class
+        public static void TryAddTransient<TService, TImplementation>(this IServiceCollection collection)
+            where TService : class
             where TImplementation : class, TService
         {
-            if(collection == null)
+            if (collection == null)
             {
                 throw new ArgumentNullException(nameof(collection));
             }
@@ -154,7 +161,7 @@ namespace Explore.DI.Abstractions
             TryAddTransient(collection, typeof(TService), typeof(TImplementation));
         }
 
-        public static void TryAddTransient<TService>(this IServiceCollection collection,Func<IServiceProvider,TService> implementationFactory)
+        public static void TryAddTransient<TService>(this IServiceCollection collection, Func<IServiceProvider, TService> implementationFactory)
             where TService : class
         {
             collection.TryAdd(ServiceDescriptor.Transient(implementationFactory));
@@ -162,9 +169,9 @@ namespace Explore.DI.Abstractions
         #endregion
 
         #region scoped
-        public static IServiceCollection AddScoped(this IServiceCollection collection,Type serviceType,Type implementationType)
+        public static IServiceCollection AddScoped(this IServiceCollection collection, Type serviceType, Type implementationType)
         {
-            if(collection == null)
+            if (collection == null)
             {
                 throw new ArgumentNullException(nameof(collection));
             }
@@ -182,30 +189,232 @@ namespace Explore.DI.Abstractions
             return Add(collection, serviceType, implementationType, ServiceLifetime.Scoped);
         }
 
-        public static IServiceCollection AddScoped(this IServiceCollection collection,Type serviceType,Func<IServiceProvider,object> implementationFactory)
+        public static IServiceCollection AddScoped(this IServiceCollection collection, Type serviceType, Func<IServiceProvider, object> implementationFactory)
         {
             if (collection == null)
             {
                 throw new ArgumentNullException(nameof(collection));
             }
-            if(serviceType == null)
+            if (serviceType == null)
             {
                 throw new ArgumentNullException(nameof(serviceType));
             }
-            if(serviceType == null)
+            if (serviceType == null)
             {
                 throw new ArgumentNullException(nameof(implementationFactory));
             }
 
-            return Add(collection, serviceType, implementationFactory,ServiceLifetime.Scoped);
+            return Add(collection, serviceType, implementationFactory, ServiceLifetime.Scoped);
+        }
+
+        public static IServiceCollection AddScoped<TService, TImplementation>(this IServiceCollection collection)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            return collection.AddScoped(typeof(TService), typeof(TImplementation));
+        }
+
+        public static IServiceCollection AddScoped(this IServiceCollection collection, Type serviceType)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException(nameof(serviceType));
+            }
+
+            return collection.AddScoped(serviceType, serviceType);
+        }
+
+        public static IServiceCollection AddScoped<TService>(this IServiceCollection collection)
+            where TService : class
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+            return collection.AddScoped(typeof(TService));
+        }
+
+        public static IServiceCollection AddScoped<TService>(this IServiceCollection collection, Func<IServiceProvider, TService> implementationFactory)
+            where TService : class
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (implementationFactory == null)
+            {
+                throw new ArgumentNullException(nameof(implementationFactory));
+            }
+
+            return collection.AddScoped(typeof(TService), implementationFactory);
+        }
+
+        public static IServiceCollection AddScoped<TService, TImplementation>(this IServiceCollection collection, Func<IServiceProvider, TImplementation> implementationFactory)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+            if (implementationFactory == null)
+            {
+                throw new ArgumentNullException(nameof(implementationFactory));
+            }
+            return collection.AddScoped(typeof(TService), implementationFactory);
         }
         #endregion
 
-        private static IServiceCollection Add(IServiceCollection collection,Type serviceType,Func<IServiceProvider,object> implementationFactory,ServiceLifetime lifetime)
+        #region Singleton
+        public static IServiceCollection AddSingleton(this IServiceCollection collection, Type serviceType, Type implementationType)
         {
-            var descriptor = new ServiceDescriptor(serviceType, implementationFactory, lifetime);
-            collection.Add(descriptor);
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException(nameof(serviceType));
+            }
+
+            if (implementationType == null)
+            {
+                throw new ArgumentNullException(nameof(implementationType));
+            }
+            return Add(collection, serviceType, implementationType, ServiceLifetime.Singleton);
+        }
+
+        public static IServiceCollection AddSingleton(this IServiceCollection collection, Type serviceType, Func<IServiceProvider, object> implementationFactory)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException(nameof(serviceType));
+            }
+            if (implementationFactory == null)
+            {
+                throw new ArgumentNullException(nameof(implementationFactory));
+            }
+            return Add(collection, serviceType, implementationFactory, ServiceLifetime.Singleton);
+        }
+
+        public static IServiceCollection AddSingleton<TService, TImplementation>(this IServiceCollection collection)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+            return collection.AddSingleton(typeof(TService), typeof(TimeoutException));
+        }
+
+        public static IServiceCollection AddSingleton(this IServiceCollection collection, Type serviceType)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException(nameof(serviceType));
+            }
+            return collection.AddSingleton(serviceType, serviceType);
+        }
+
+        public static IServiceCollection AddSingleton<TService>(this IServiceCollection collection)
+            where TService : class
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+            return collection.AddSingleton(typeof(TService));
+        }
+
+        public static IServiceCollection AddSingleton<TService>(this IServiceCollection collection, Func<IServiceProvider, TService> implementationFactory)
+            where TService : class
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+            if (implementationFactory == null)
+            {
+                throw new ArgumentNullException(nameof(implementationFactory));
+            }
+
+            return collection.AddSingleton(typeof(TService), implementationFactory);
+        }
+
+        public static IServiceCollection AddSingleton<TService, TImplementation>(this IServiceCollection collection, Func<IServiceProvider, TImplementation> implementationFactory)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (implementationFactory == null)
+            {
+                throw new ArgumentNullException(nameof(implementationFactory));
+            }
+            return collection.AddSingleton(typeof(TService), implementationFactory);
+        }
+
+        public static IServiceCollection AddSingleton(this IServiceCollection collection, Type serviceType, object implementationInstance)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException(nameof(serviceType));
+            }
+
+            if (implementationInstance == null)
+            {
+                throw new ArgumentNullException(nameof(implementationInstance));
+            }
+
+            var serviceDescriptor = new ServiceDescriptor(serviceType, implementationInstance);
+            collection.Add(serviceDescriptor);
             return collection;
         }
+
+        public static IServiceCollection AddSingleton<TService>(this IServiceCollection collection,TService implementationInstance)
+            where TService : class
+        {
+            if(collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+            if(implementationInstance == null)
+            {
+                throw new ArgumentNullException(nameof(implementationInstance));
+            }
+            return collection.AddSingleton(typeof(TService), implementationInstance);
+        }
+        #endregion
+              
     }
 }
