3d458b3edc75d938fcbc477266670a72984b7202
liuchao <ry2u@outlook.com>
2018-03-28 23:14:48 +0800

    callsite

---
+++ Explore.DI.Abstractions/Explore.DI/ServiceLookup/ConstructorCallSite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace Explore.DI.ServiceLookup
+{
+    internal class ConstructorCallSite : IServiceCallSite
+    {
+        internal ConstructorInfo ConstructorInfo { get; }
+        internal IServiceCallSite[] ParameterCallSites { get; }
+
+        public ConstructorCallSite(Type serviceType, ConstructorInfo construtorInfo,IServiceCallSite[] parameterCallSites)
+        {
+            ServiceType = serviceType;
+            ConstructorInfo = construtorInfo;
+            ParameterCallSites = parameterCallSites;
+            ParameterCallSites = parameterCallSites;
+        }
+
+        public Type ServiceType { get; }
+        public Type ImplementationType => ConstructorInfo.DeclaringType;
+    }
+}
---
+++ Explore.DI.Abstractions/Explore.DI/ServiceLookup/FactoryCallSite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Explore.DI.ServiceLookup
+{
+    internal class FactoryCallSite : IServiceCallSite
+    {
+        public Func<IServiceProvider,object> Factory { get; }
+        public Type ServiceType { get; }
+        public Type ImplementationType => null;
+        public FactoryCallSite(Type serviceType,Func<IServiceProvider,object> factory)
+        {
+            Factory = factory;
+            ServiceType = serviceType;
+        }
+    }
+}
