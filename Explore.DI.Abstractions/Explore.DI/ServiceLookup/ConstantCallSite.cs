e8f53f10f40131357fff62b6766f55474e03e9fe
liuchao <ry2u@outlook.com>
2018-03-31 23:24:41 +0800

    ConstantCallSite

---
+++ Explore.DI.Abstractions/Explore.DI/ServiceLookup/ConstantCallSite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Explore.DI.ServiceLookup
+{
+    internal class ConstantCallSite : IServiceCallSite
+    {
+        internal object DefaultValue { get; }
+        public ConstantCallSite(Type serviceType,object defaultVaule)
+        {
+            DefaultValue = defaultVaule;
+        }
+        public Type ServiceType => DefaultValue.GetType();
+        public Type ImplementationType => DefaultValue.GetType();
+    }
+}
