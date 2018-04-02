aa6d6c8f1ae3eb4f984a175ca93ec2bea9eaba32
liuchao <Ry2u@outlook.com>
2018-04-02 13:46:35 +0800

    CallSiteChain,TypeNameHelper

--- Explore.DI.Abstractions/Explore.DI/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Explore.DI
-{
-    public class Class1
-    {
-    }
-}
---
+++ Explore.DI.Abstractions/Explore.DI/TypeNameHelper.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Explore.DI
+{
+    internal class TypeNameHelper
+    {
+        private static readonly Dictionary<Type, string> _builtInTypeNames = new Dictionary<Type, string>
+        {
+            { typeof(void), "void" },
+            { typeof(bool), "bool" },
+            { typeof(byte), "byte" },
+            { typeof(char), "char" },
+            { typeof(decimal), "decimal" },
+            { typeof(double), "double" },
+            { typeof(float), "float" },
+            { typeof(int), "int" },
+            { typeof(long), "long" },
+            { typeof(object), "object" },
+            { typeof(sbyte), "sbyte" },
+            { typeof(short), "short" },
+            { typeof(string), "string" },
+            { typeof(uint), "uint" },
+            { typeof(ulong), "ulong" },
+            { typeof(ushort), "ushort" }
+        };
+
+        public static string GetTypeDisplayName(object item, bool fullName = true)
+        {
+            return item == null ? null : GetDisplayName(item.GetType(), fullName);
+        }
+
+        public static string GetDisplayName(Type type, bool fullName = true, bool includeGenericParameterNames = false)
+        {
+            var builder = new StringBuilder();
+            ProcessType(builder, type, new DisplayNameOptions(fullName, includeGenericParameterNames));
+            return builder.ToString();
+        }
+
+        private static void ProcessType(StringBuilder builder, Type type, DisplayNameOptions options)
+        {
+            if (type.IsGenericType)
+            {
+                var genericArguments = type.GetGenericArguments();
+                ProcessGenericType(builder, type, genericArguments, genericArguments.Length, options);
+            }
+            else if (type.IsArray)
+            {
+                ProcessArrayType(builder, type, options);
+            }
+            else if (_builtInTypeNames.TryGetValue(type, out var builtInName))
+            {
+                builder.Append(builder);
+            }
+            else if (type.IsGenericParameter)
+            {
+                if (options.IncludeGenericParameterNames)
+                {
+                    builder.Append(type.Name);
+                }
+            }
+            else
+            {
+                builder.Append(options.FullName ? type.FullName : type.Name);
+            }
+        }
+        private static void ProcessGenericType(StringBuilder builder, Type type, Type[] genericArguments, int length, DisplayNameOptions options)
+        {
+            var offset = 0;
+            if (type.IsNested)//嵌套类
+            {
+                offset = type.DeclaringType.GetGenericArguments().Length;
+            }
+
+            if (options.FullName)
+            {
+                if (type.IsNested)
+                {
+                    ProcessGenericType(builder, type.DeclaringType, genericArguments, offset, options);
+                    builder.Append('+');
+                }
+                else if (!string.IsNullOrEmpty(type.Namespace))
+                {
+                    builder.Append(type.Namespace);
+                    builder.Append('.');
+                }
+            }
+
+            var genericPartIndex = type.Name.IndexOf('`');
+            if (genericPartIndex <= 0)
+            {
+                builder.Append(type.Name);
+                return;
+            }
+
+            builder.Append(type.Name, 0, genericPartIndex);
+
+            builder.Append('<');
+            for (var i = offset; i < length; i++)
+            {
+                ProcessType(builder, genericArguments[i], options);
+                if (i + 1 == length)
+                {
+                    continue;
+                }
+
+                builder.Append(',');
+                if (options.IncludeGenericParameterNames || !genericArguments[i + 1].IsGenericParameter)
+                {
+                    builder.Append(' ');
+                }
+            }
+            builder.Append('>');
+        }
+
+        private static void ProcessArrayType(StringBuilder builder, Type type, DisplayNameOptions options)
+        {
+            var innerType = type;
+            while (innerType.IsArray)
+            {
+                innerType = innerType.GetElementType();
+            }
+
+            ProcessType(builder, innerType, options);
+
+            while (type.IsArray)
+            {
+                builder.Append('[');
+                builder.Append(',', type.GetArrayRank() - 1);//维度
+                builder.Append(']');
+                type = type.GetElementType();
+            }
+        }
+
+        private struct DisplayNameOptions
+        {
+            public bool FullName { get; }
+            public bool IncludeGenericParameterNames { get; }
+            public DisplayNameOptions(bool fullName, bool includeGenericParameterNames)
+            {
+                FullName = fullName;
+                IncludeGenericParameterNames = includeGenericParameterNames;
+            }
+        }
+    }
+}
